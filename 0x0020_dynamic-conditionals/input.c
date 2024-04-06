7ef77e14282fd5dbe74f67c627bfd0f437d13149
Kevin Thomas <kevin@mytechnotalent.com>
2024-04-06 16:31:06 -0400

    added chapter 32, 33 and 34

---
+++ 0x0020_dynamic-conditionals/input.c
@@ -0,0 +1,52 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2024 My Techno Talent
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+/** @file input.c
+ * 
+ *  @brief Functions for the input driver.
+ * 
+ *  This contains the functions for the input driver which will handle UART
+ *  configuration in addition to capturing a character from the UART terminal.
+ *   
+ *  @author Kevin Thomas
+ *  @date   04/06/2024
+*/
+
+#include "input.h"
+
+void uart0_init(void)
+{
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+}
+
+uint8_t on_uart_rx(void) 
+{
+    uint8_t c = 0;
+    while (!uart_is_readable(UART_ID)) {
+        c = uart_getc(UART_ID);
+        return c;
+    }
+}
