9bf6113b1c596754a45ffd5d677bc9e63a6630ec
Kevin Thomas <kevin@mytechnotalent.com>
2025-09-19 17:52:20 -0700

    Update chap 8

--- 0x0008_unitialized-variables/0x0008_unitialized-variables.c
+++ 0x0008_unitialized-variables/0x0008_unitialized-variables.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN 25 
+
 int main(void) {
     uint8_t age;
 
     stdio_init_all();
 
-    while (true)
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
         printf("age: %d\r\n", age);
+
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+    }
 }
