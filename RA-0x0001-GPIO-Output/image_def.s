ce213ab164a7e029149f188a473cfa71281f471d
Kevin Thomas <kevin@mytechnotalent.com>
2025-10-25 17:25:25 -0400

    Add files via upload

--- RA-0x0001-GPIO-Output/image_def.s
+++ RA-0x0001-GPIO-Output/image_def.s
@@ -20,14 +20,14 @@
  * UPDATE DATE: October 5, 2025
  */
 
-.section .picobin_block, "a"                               // place IMAGE_DEF block in flash
+.section .picobin_block, "a"                     // place IMAGE_DEF block in flash
 
-.word  0xFFFFDED3                                          // PICOBIN_BLOCK_MARKER_START
-.byte  0x42                                                // PICOBIN_BLOCK_ITEM_1BS_IMAGE_TYPE
-.byte  0x1                                                 // item is 1 word in size
-.hword 0b0001000000100001                                  // SECURE mode (0x1021)
-.byte  0xFF                                                // PICOBIN_BLOCK_ITEM_2BS_LAST
-.hword 0x0001                                              // item is 1 word in size
-.byte  0x0                                                 // pad
-.word  0x0                                                 // relative pointer to next block (0 = loop to self)
-.word  0xAB123579                                          // PICOBIN_BLOCK_MARKER_END
+.word  0xFFFFDED3                                // PICOBIN_BLOCK_MARKER_START
+.byte  0x42                                      // PICOBIN_BLOCK_ITEM_1BS_IMAGE_TYPE
+.byte  0x1                                       // item is 1 word in size
+.hword 0b0001000000100001                        // SECURE mode (0x1021)
+.byte  0xFF                                      // PICOBIN_BLOCK_ITEM_2BS_LAST
+.hword 0x0001                                    // item is 1 word in size
+.byte  0x0                                       // pad
+.word  0x0                                       // relative pointer to next block (0 = loop to self)
+.word  0xAB123579                                // PICOBIN_BLOCK_MARKER_END
